fe4dff965cd88642b05ed1a738f4d689ce5ac9db
innesbre <brendan.innes@mail.utoronto.ca>
2018-06-14 13:05:34 -0400

    Starting the 'select from plot for DE' idea.

--- app.R
+++ app.R
@@ -161,6 +161,12 @@ ui <- fixedPage(
     column(6,align="left",downloadButton("goiPlot1Save","Save as PDF")),
     column(6,align="right",downloadButton("goiPlot2Save","Save as PDF"))
   ),
+  hr(),
+  
+  #### Targeted DE ####
+  fixedRow(
+    column(6,plotOutput("tsneForSelect",height="570px"))
+  ),
   h1()
 )
 
@@ -913,6 +919,16 @@ server <- function(input,output,session) {
       dev.off()
     }
   )
+  
+  
+  #### Targeted DE ####
+  output$tsneForSelect <- renderPlot({
+    if (length(res()) > 0) {
+      print(plot_tsne())
+      print(plot_tsne_labels())
+    }
+  })
+  
   
 }
 
