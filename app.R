85a04d11e6b565d6f4dd1064f7431c15f7bcdd2b
innesbre <brendan.innes@mail.utoronto.ca>
2018-06-14 13:45:01 -0400

    Revert "Merge branch 'TargDE' into dev"
    This reverts commit e16a2aedf6cd72cc71c10f4ad2c85b16ea117224, reversing
    changes made to db59c117938e359d5d0a48e57c55c44346a44304.

--- app.R
+++ app.R
@@ -164,12 +164,6 @@ ui <- fixedPage(
     column(6,align="left",downloadButton("goiPlot1Save","Save as PDF")),
     column(6,align="right",downloadButton("goiPlot2Save","Save as PDF"))
   ),
-  hr(),
-  
-  #### Targeted DE ####
-  fixedRow(
-    column(6,plotOutput("tsneForSelect",height="570px"))
-  ),
   h1()
 )
 
@@ -923,16 +917,6 @@ server <- function(input,output,session) {
       dev.off()
     }
   )
-  
-  
-  #### Targeted DE ####
-  output$tsneForSelect <- renderPlot({
-    if (length(res()) > 0) {
-      print(plot_tsne())
-      print(plot_tsne_labels())
-    }
-  })
-  
   
 }
 
