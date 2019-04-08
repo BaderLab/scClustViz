2ffd25a725cbd31dfb42c2a0951779035d0db8c6
innesbre <brendan.innes@mail.utoronto.ca>
2019-04-08 08:00:04 -0400

    Demo app deployment on shinyapps.io

--- inst/scClustVizDemoApp/app.R
+++ inst/scClustVizDemoApp/app.R
@@ -1,3 +1,6 @@
 # Put something here! This is needed when publishing the package to a Shiny Server, and
 # should refer to a function that returns a shiny app object. See
 # https://github.com/MangoTheCat/shinyAppDemo
+
+library(MouseCortex)
+viewMouseCortex("e13")
